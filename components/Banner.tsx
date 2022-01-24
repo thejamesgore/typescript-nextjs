ad1ce563357c03ce8030b784f9f9f2e1b86a3a57
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-01-24 16:51:11 +0000

    server side rendering

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -7,7 +7,7 @@ function Banner() {
           <h1 className="text-6xl max-w-xl font-serif   "><span className="underline decoration-black decoration-4">Medium</span> is a place to write, read, and connect</h1>
           <h2>It's easy and free to post your thinking on any topic and connect with millions of readers.</h2>
       </div>
-      <img className="hidden md:inline-flex h-32 lg:h-full" src="/images/Medium-logo.png" alt="medium logo" />
+      <img className="hidden md:inline-flex h-32 lg:h-full" src="https://github.com/thejamesgore/typescript-nextjs/blob/main/images/Medium-logo.png?raw=true" alt="medium logo" />
   </div>
   )
 }
